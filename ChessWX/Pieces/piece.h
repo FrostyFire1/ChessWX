2f4b64be65aad04886c4e99476288552dd712d53
FrostyFire1 <kamilbaron02@gmail.com>
2022-11-22 22:22:11 +0100

    remove useless f() function from piece

--- ChessWX/Pieces/piece.h
+++ ChessWX/Pieces/piece.h
@@ -2,7 +2,6 @@
 #include <string>
 class piece {
 public:
-	virtual void f();
 	virtual std::string getDisplayText(); //virtual methods can be override, great for "downcasting"
 	std::string displayText = "piece";
 };
